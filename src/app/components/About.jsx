ee21bded4ca9ca2627b0c4b9409229a7cef0d4a5
jwang9228 <jwang.srv1@gmail.com>
2025-05-25 03:00:42 -0700

    Add home page decrypt and fade animations & effects

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,8 +1,92 @@
+import Link from 'next/link';
+import { CiLocationOn } from 'react-icons/ci';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { CiMail, CiLinkedin } from 'react-icons/ci';
+import { PiPlant, PiGraduationCapLight } from 'react-icons/pi';
+import Threads from './utils/Threads';
+import DecryptedText from './utils/DecryptedText';
+import FadeContent from './utils/FadeContent';
+
 export default function Home() {
+
+  const socials = [
+    {'icon': <CiMail className='size-6.5 scale-y-125' />,  'href': 'mailto:jwang.srv1@gmail.com'},
+    {'icon': <CiLinkedin className='size-7.5' />, 'href': 'https://www.linkedin.com/in/justin-n-wang/'},
+    {'icon': <AiOutlineGithub className='size-6.5' />, 'href': 'https://github.com/jwang9228/'}
+  ];
+
   return (
-    <div className='flex flex-col'>
-      <div className='text-3xl font-medium'>Justin Wang</div>
-      <div className='text-lg mt-1 text-zinc-300'>Software Developer</div>
+    <div className='flex flex-col gap-y-10'>
+      <div className='absolute -z-10 mt-5 opacity-50'>
+        <FadeContent delay={100} duration={2500}>
+          <Threads
+            amplitude={0.4}
+            distance={0}
+            enableMouseInteraction={false}
+          />
+        </FadeContent>
+      </div>
+      <div className='flex flex-col gap-y-1'>
+        <FadeContent>
+          <DecryptedText 
+            text='Justin Wang' 
+            parentClassName='text-3xl tablet:text-4xl font-medium'
+            speed={65}
+            maxIterations={15}
+            sequential
+            useOriginalCharsOnly
+            animateOn='view'
+          />
+        </FadeContent>
+        <FadeContent delay={50}>
+          <DecryptedText 
+            text='Full Stack Developer' 
+            parentClassName='text-lg text-zinc-300'
+            speed={30}
+            maxIterations={15}
+            sequential
+            useOriginalCharsOnly
+            animateOn='view'
+          />
+        </FadeContent>
+      </div>
+      <div className='flex flex-col gap-y-2 mt-4 text-zinc-300'>
+        <FadeContent delay={150} duration={2000}>
+          <div className='flex items-center gap-x-3'>
+            <PiPlant className='size-4' />
+            Current: SDE @ Agrofocal
+          </div>
+        </FadeContent>
+        <FadeContent delay={200} duration={2000}>
+          <div className='flex items-center gap-x-3'>
+            <CiLocationOn className='size-4' />
+            Pleasanton, CA
+          </div>
+        </FadeContent>
+        <FadeContent delay={250} duration={2000}>
+          <div className='flex items-start gap-x-3'>
+            <PiGraduationCapLight className='size-4 mt-1' />
+            <div className='flex flex-col'>
+              <div>Northeastern University '23</div>
+              <div>B.S. in Computer Science</div>
+            </div>
+          </div>
+        </FadeContent>
+      </div>
+      <FadeContent delay={400} duration={2000}>
+        <div className='flex items-center gap-x-4.5'>
+          {socials.map((social, i) => (
+            <Link
+              key={i}
+              href={social.href}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {social.icon}
+            </Link>
+          ))}
+        </div>
+      </FadeContent>
     </div>
   )
 }
--- src/app/components/RootLayout.jsx
+++ src/app/components/RootLayout.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import FadeContent from './utils/FadeContent';
 
 export default function RootLayout({children}) {
   const [activeTab, setActiveTab] = useState('about');
@@ -20,50 +21,54 @@ export default function RootLayout({children}) {
   ];
 
   return (
-    <div className='flex flex-col justify-between h-screen px-8 tablet:px-10 pt-6 pb-4'>
+    <div className='flex flex-col justify-between h-screen px-8 tablet:px-14 laptop:px-16 pt-6 pb-6 tablet:pb-8'>
       <div>
-        <div className='flex flex-wrap justify-end items-center gap-x-5 tablet:gap-x-7'>
-          {tabs.map((tab, i) => (
-            <Link 
-              key={i} 
-              href={tab.href} 
-              onClick={() => setActiveTab(tab.label)}
-              className={`group transition duration-200 text-lg tablet:text-xl text-foreground/75
-                ${activeTab === tab.label && 'text-foreground/100'}`
-              }
-            >
-              {tab.label}
-              <span 
-                className={`block h-0.5 bg-foreground/85
-                  ${activeTab === tab.label 
-                    ? 'max-w-full' 
-                    : 'max-w-0 group-hover:max-w-full transition-all duration-300'
-                  }`
-                } 
-              />
-            </Link>
-          ))}
-        </div>
-        <div className='py-8'>
+        <FadeContent duration={750}>
+          <div className='flex flex-wrap justify-end items-center gap-x-5 tablet:gap-x-7'>
+            {tabs.map((tab, i) => (
+              <Link 
+                key={i} 
+                href={tab.href} 
+                onClick={() => setActiveTab(tab.label)}
+                className={`group transition duration-200 text-lg tablet:text-xl text-foreground/75
+                  ${activeTab === tab.label && 'text-foreground/100'}`
+                }
+              >
+                {tab.label}
+                <span 
+                  className={`block h-0.5 bg-foreground/80
+                    ${activeTab === tab.label 
+                      ? 'max-w-full' 
+                      : 'max-w-0 group-hover:max-w-full transition-all duration-300'
+                    }`
+                  } 
+                />
+              </Link>
+            ))}
+          </div>
+        </FadeContent>
+        <div className='py-10'>
           {children}
         </div>
       </div>
-      <div className='flex flex-col gap-y-2.5'>
-        <hr className='text-zinc-300/70' />
-        <div className='flex flex-wrap items-center gap-x-6 tablet:gap-x-8'>
-          {socials.map((social, i) => (
-            <Link
-              key={i}
-              href={social.href}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-foreground/90 laptop:text-foreground/80 hover:text-foreground/100 text-lg'
-            >
-              {social.label}
-            </Link>
-          ))}
+      <FadeContent duration={750}>
+        <div className='flex w-fit flex-col gap-y-4'>
+          <hr className='text-zinc-300/70' />
+          <div className='flex items-center gap-x-6 tablet:gap-x-8'>
+            {socials.map((social, i) => (
+              <Link
+                key={i}
+                href={social.href}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-foreground/90 laptop:text-foreground/80 hover:text-foreground/100 text-lg'
+              >
+                {social.label}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
+      </FadeContent>
     </div>
   )
 }
