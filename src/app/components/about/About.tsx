4e1ebb18f3f9a6bddf24e1abba5c4fd4a28580ed
jwang9228 <jwang.srv1@gmail.com>
2026-04-06 18:48:00 -0700

    Add initial experimental grid layout for about section

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -3,14 +3,26 @@ import Fade from '../utils/Fade';
 export default function About() {
   return (
     <Fade type='up' as='div' inView 
-      className='flex flex-col gap-base layout-px layout-py'
+      className='grid grid-cols-1 tablet:grid-cols-3 gap-lg auto-rows-auto tablet:auto-rows-[384px] layout-px layout-py'
     >
-      <div>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      {/* TODO: what animations to use? */} 
+      {/* ROW 1 */}
+      <div className='laptop:col-span-2 border border-background/10 rounded-2xl p-base'>
+        Row 1 Column 1 (Bio - 2/3 width)
       </div>
-      <div>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </div>    
+      
+      <div className='laptop:col-span-1 border border-background/10 rounded-2xl p-base'>
+        Row 1 Column 2 (Games - 1/3 width)
+      </div>
+
+      {/* ROW 2 */}
+      <div className='laptop:col-span-1 border border-background/10 rounded-2xl p-base'>
+        Row 2 Column 1 (Active - 1/3 width)
+      </div>
+      
+      <div className='laptop:col-span-2 border border-background/10 rounded-2xl p-base'>
+        Row 2 Column 2 (PC Build - 2/3 width)
+      </div>
     </Fade>
   )
 }
